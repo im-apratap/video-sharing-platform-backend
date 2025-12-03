f0419847aa494b41691a37957e09504128f91e63
Aaditya Pratap <aadityapratapiit@gmail.com>
2025-12-04 01:37:29 +0530

    made routes and controllers of register, login and logout

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { User } from "../models/user.model.js"
 import { uploadToCloudinary } from "../utils/cloudinary.js"
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try{
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+
+    }catch(error){
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async(req,res)=>{
     // get user details from frontend
     // validation - not empty
@@ -35,7 +51,7 @@ const registerUser = asyncHandler(async(req,res)=>{
     // const coverImageLocalPath = req.files?.coverImage[0]?.path
 
     let coverImageLocalPath
-    if(req.files & Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
         coverImageLocalPath = req.files.coverImage[0].path
     }
 
@@ -68,8 +84,80 @@ const registerUser = asyncHandler(async(req,res)=>{
     }
 
     return res.status(201).json(
-        new ApiResponse(200,createdUser,"User registered successfully")
+        new ApiResponse(201,createdUser,"User registered successfully")
     )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async(req,res)=>{
+    /* req body -> data
+    username or email
+    find the user
+    password check
+    access and refresh token
+    send cookie */
+
+    const {email, username, password} = req.body
+
+    if(!username || !email){
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User doesn't exists")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true, // by this cookies wil only be modifiable by server. anyone can see that but can't modify it
+        secure: true
+    }
+
+    return  res.status(200)
+    .cookie("acessToken",accessToken, options)
+    .cookie("refreshToken",refreshToken, options)
+    .json(
+        new ApiResponse(200,{
+            user: loggedInUser, accessToken, refreshToken
+        },"User logged in successfully")
+    )
+
+})
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(req.user._id,{
+        $set: {
+            refreshToken: undefined
+        }
+    },
+    {
+        new: true
+    }
+)
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken")
+    .clearCookie("refreshToken")
+    .json(new ApiResponse(200, {}, "User logged out successfully"))
+
+})
+
+export {registerUser, loginUser, logoutUser}
