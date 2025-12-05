f73976feb448744c83dcb6e3549600bc3c01e3d0
Aaditya Pratap <aadityapratapiit@gmail.com>
2025-12-05 20:06:44 +0530

    added getUserChannelProfile function and updated user and subscription models

--- backend/src/models/subscription.model.js
+++ backend/src/models/subscription.model.js
@@ -1,6 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({
+const subscriptionSchema = new Schema({
     subscriber: {
         type: Schema.Types.ObjectId, // one who is subscribing
         ref: "User"
